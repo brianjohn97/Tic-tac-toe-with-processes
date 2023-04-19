f6e1813872767848a589fa64a5289696c64029d6
brianjohn97 <31637076+brianjohn97@users.noreply.github.com>
2023-04-19 15:43:51 -0700

    added printboard function and command line arguements to get grid size with checks.

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
-using namespace std;
+#include <unistd.h>
 
-int main() {
-    std::cout << "hell world";
-    cout << "adfh";
+using namespace std;
+int gridSize;
+char board[3][3] = {{'-','-','-'},{'-','-','-'},{'-','-','-'}};
+char board2[3][3] = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
+string playerOne = "X";
+string playerTwo = "O";
+
+void printBoard(){
+    for (int i = 0; i < gridSize; i++){
+        for (int j = 0; j < gridSize; j++){
+            cout << "[" << board [i][j] << "]";
+        }
+        cout << endl;
+    }
+    
+}
+void checkWinner(){
+
+}
+
+int main(int argc, char * argv[]) {
+
+    if(argc < 2){
+        cout << "Grid size was not specified. Please try again with a grid size like 3 for example." << endl;
+        exit(0);
+    }
+    try{
+        gridSize = stoi(argv[1]);
+    }catch (exception &err){
+        cout << "Please enter the grid size in decimal format and try again." << endl;
+        exit(0);
+    }
+    cout << "Player one is: " << playerOne << endl;
+    cout << "Player two is: " << playerTwo << endl;
 }
